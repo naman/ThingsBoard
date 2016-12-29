1012be70451be898965fdc4c1f658ad97b1ef389
naman <namanspace@gmail.com>
2016-12-29 23:51:36 +0530

    added todo's

--- thingsboard/app/views.py
+++ thingsboard/app/views.py
@@ -14,4 +14,15 @@ def things(request):
                 'description': 'Bulb', 'events': ['Off state']}]
     # read the log file and update context dictionary
     # add to the events array
+    f = open('/var/log/snort/alert')
+    for x in f.readlines():
+        split = x.split()
+        time_stamp = split[0]
+        packet_type = split[3]
+        packet_method = split[4]
+        destination = split[len(split) - 1]
+
+        event = time_stamp + "\t" + packet_type +  "\t" + packet_method + " with " + destination 
+        context[0]['events'].append(event)
+
     return render(request, 'app/things.html', {'things': context})
