134fdde8f9289732e545473c43fe773829f92f4f
naman <namanspace@gmail.com>
2017-01-30 20:00:22 +0530

    added blank functions for detection

--- thingsboard/app/urls.py
+++ thingsboard/app/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index),
-    url(r'^things/$', views.things)
+    url(r'^things/$', views.things),
+    url(r'^detect/$', views.detect),
+    url(r'^owner/$', views.owner)
 ]
