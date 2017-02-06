c048815b9a612b7ac47164b509565532a32e5458
naman <namanspace@gmail.com>
2017-02-06 17:02:43 +0530

    Changed Models

--- thingsboard/app/models.py
+++ thingsboard/app/models.py
@@ -1,15 +1,43 @@
 from django.db import models
 
 
+class Connections(models.Model):
+    source_address = models.CharField("Source Address", max_length=120)
+    destination_address = models.CharField(
+        "Destination Address", max_length=120)
+    type_of_connection = models.CharField("Connection Type", max_length=120)
+    visited = models.DateTimeField(auto_now_add=True)
+
+
+class Permissions(models.Model):
+    name = models.CharField("Name", max_length=120)
+
+
+class URL(models.Model):
+    name = models.CharField("Name", max_length=120)
+    visited = models.DateTimeField(auto_now_add=True)
+
+
+class State(models.Model):
+    name = models.CharField("Name", max_length=120)
+    visited = models.DateTimeField(auto_now_add=True)
+
+
 class Things(models.Model):
 
     """Internet of Things"""
 
-    name = models.CharField("Name", max_length=120)
-    description = models.CharField("Description", max_length=120)
-    mac_address = models.CharField("MAC Address", max_length=120, null=True)
-    state = models.CharField("State", max_length=120)
-    # location =
+    urls_visited = models.ForeignKey(URL)
+    permissions = models.ForeignKey(Permissions)
+    connections = models.ForeignKey(Connections)
+    state = models.ForeignKey(State)
 
-# def __str__(self):  # __unicode__ on Python 2
-    # return self.name
+    name = models.CharField("Name", max_length=120)
+    description = models.CharField("Description", max_length=120, null=True)
+    mac_address = models.CharField("MAC Address", max_length=120)
+    ip_address = models.CharField("IP address", max_length=120)
+    location = models.CharField("Location", max_length=120, null=True)
+    admin_or_not = models.BooleanField(default=0)
+
+    def __str__(self):  # __unicode__ on Python 2
+        return self.name
