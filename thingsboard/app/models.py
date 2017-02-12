15d0c1e54355ed24b8238ba6c80ccbd5395bf9ad
naman <namanspace@gmail.com>
2017-02-12 23:22:43 +0530

    add new device found in DB if not already present

--- thingsboard/app/models.py
+++ thingsboard/app/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 
 class Connections(models.Model):
+    # conection_id = models.AutoField() # already there
     source_address = models.CharField("Source Address", max_length=120)
     destination_address = models.CharField(
         "Destination Address", max_length=120)
@@ -10,34 +11,35 @@ class Connections(models.Model):
 
 
 class Permissions(models.Model):
-    name = models.CharField("Name", max_length=120)
+    name = models.CharField("Name", primary_key=True, max_length=120)
 
 
 class URL(models.Model):
-    name = models.CharField("Name", max_length=120)
+    name = models.CharField("Name", primary_key=True, max_length=120)
     visited = models.DateTimeField(auto_now_add=True)
 
 
 class State(models.Model):
-    name = models.CharField("Name", max_length=120)
+    name = models.CharField("Name", primary_key=True, max_length=120)
     visited = models.DateTimeField(auto_now_add=True)
 
 
-class Things(models.Model):
+class Thing(models.Model):
 
     """Internet of Things"""
 
-    urls_visited = models.ForeignKey(URL)
-    permissions = models.ForeignKey(Permissions)
-    connections = models.ForeignKey(Connections)
-    state = models.ForeignKey(State)
+    urls_visited = models.ForeignKey(URL, null=True)
+    permissions = models.ForeignKey(Permissions, null=True)
+    connections = models.ForeignKey(Connections, null=True)
+    state = models.ForeignKey(State, null=True)
 
     name = models.CharField("Name", max_length=120)
     description = models.CharField("Description", max_length=120, null=True)
-    mac_address = models.CharField("MAC Address", max_length=120)
+    mac_address = models.CharField(
+        "MAC Address", max_length=120)
     ip_address = models.CharField("IP address", max_length=120)
     location = models.CharField("Location", max_length=120, null=True)
-    admin_or_not = models.BooleanField(default=0)
+    admin_or_not = models.BooleanField(default=False)
 
     def __str__(self):  # __unicode__ on Python 2
         return self.name
--- thingsboard/app/views.py
+++ thingsboard/app/views.py
@@ -1,45 +1,82 @@
 from django.shortcuts import render
-from django.http import HttpResponseRedirect
-from django.db import connection
+import random
+import string
+from app.models import *
+
+
+def fetch_ip(mac):
+    f = open('/proc/net/arp')
+    ip = ""
+    for x in f.readlines():
+        if mac in x:
+            ip = x.split()[0].strip()
+            break
+    return ip
 
 
 def index(request):
     return render(request, 'app/index.html')
 
-def detect(request):
+
+def owner(request):
+    '''
+    # get owner from the DB, if current request is from owner, show this page
+    # else redirect to home
+    '''
+    return render(request, 'app/owner.html')
+
+
+def block_url(url, source_device_ip):
+    '''
+    # take url,
+    # get its ip from DNS
+    # convert ip to hex to suit snort format
+    # make rule with hostname as source device's ip
+    # give a unique sid for the rule
+    '''
+    f = open("/etc/snort/rules/local.rules", 'a')
+    rule = ""
+    f.write(rule)
+
+
+def things(request):
+    '''
+    # read the log file and update context dictionary
+    # add to the events array
+    '''
+
+    # f = open('/var/log/snort/alert')
+    # for x in f.readlines():
+    #     split = x.split()
+    #     time_stamp = split[0]
+    #     packet_type = split[3]
+    #     packet_method = split[4]
+    #     destination = split[len(split) - 1]
+
+    #     event = time_stamp + "\t" + packet_type + \
+    #         "\t" + packet_method + " with " + destination
+    #     context[0]['events'].append(x)
+
+    '''
     # detect new devices
     # check them in DB
     # if not present, add device in DB
-    logfile = open("/home/pi/Documents/BTP/ap.log", "r")
-    for line in logfile.readlines():
+    '''
+
+    f = open("../../ap.log")
+    for line in f.readlines():
         if ("AP-STA-CONNECTED" in line):
-            print "New device connected"
-            spl = line.split(" ")
-            print "MAC: " + spl[3]
-    return render(request, 'app/detect_devices.html')
-    
-def owner(request):
-    # get owner from the DB, if current request is from owner, show this page
-    # else redirect to home 
-    return render(request, 'app/owner.html')
-
-def things(request):
-    context = [{'id': 1, 'name': 'Motorola FOCUS 66',
-                'description': 'Smart Security Camera', 'events': ['']},
-               {'id': 2, 'name': 'Raspberry Pi',
-                'description': 'Gateway', 'events': ['01/06-11:29:31.458958  [**] [1:10000024:1] Possible TCP DoS [**] [Priority: 0] {TCP} 192.168.26.25:50145 -> 192.168.26.186:80']}]
-    # read the log file and update context dictionary
-    # add to the events array
-    f = open('/var/log/snort/alert')
-    for x in f.readlines():
-        split = x.split()
-        time_stamp = split[0]
-        packet_type = split[3]
-        packet_method = split[4]
-        destination = split[len(split) - 1]
-
-        event = time_stamp + "\t" + packet_type + \
-            "\t" + packet_method + " with " + destination
-        context[0]['events'].append(x)
-
+            spl = line.split()
+            mac = spl[3].strip()
+            if len(Thing.objects.filter(mac_address=mac)) < 1:
+                print "New device found!"
+                # add a new device, not already present
+                unique_name = ''.join(random.choice(
+                    string.ascii_uppercase + string.digits) for _ in range(6))
+                thing = Thing(name=unique_name, mac_address=mac,
+                              ip_address=fetch_ip(mac), admin_or_not=False)
+                thing.save()
+            else:
+                print "Device already in the DB!"
+    context = Thing.objects.all()
     return render(request, 'app/things.html', {'things': context})
