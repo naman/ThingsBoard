f00b1747f82f68f738050ad95763fbc98a9c9d29
naman <namanspace@gmail.com>
2017-04-18 14:53:22 +0530

    changed graph

--- thingsboard/app/views.py
+++ thingsboard/app/views.py
@@ -299,48 +299,34 @@ class Graph(TemplateView):
     def get_context_data(self, **kwargs):
         context = super(Graph, self).get_context_data(**kwargs)
 
-        x = [-2, 0, 4, 6, 7]
-        y = [q**2 - q + 3 for q in x]
-        trace1 = go.Scatter(x=x, y=y, marker={'color': 'red', 'symbol': 104, 'size': "10"},
-                            mode="lines", name='1st Trace')
+        df = pd.read_csv(
+            "https://raw.githubusercontent.com/plotly/datasets/master/finance-charts-apple.csv")
 
-        data = go.Data([trace1])
-        layout = go.Layout(title="Meine Daten", xaxis={
-                           'title': 'x1'}, yaxis={'title': 'x2'})
+        trace_high = go.Scatter(
+            x=df.Date,
+            y=df['AAPL.High'],
+            name="AAPL High",
+            line=dict(color='#17BECF'),
+            opacity=0.8)
+
+        trace_low = go.Scatter(
+            x=df.Date,
+            y=df['AAPL.Low'],
+            name="AAPL Low",
+            line=dict(color='#7F7F7F'),
+            opacity=0.8)
+
+        data = [trace_high, trace_low]
+
+        layout = dict(
+            title="Manually Set Date Range",
+            xaxis=dict(
+                range=['2016-07-01', '2016-12-31'])
+        )
+
         figure = go.Figure(data=data, layout=layout)
         div = opy.plot(figure, auto_open=False, output_type='div')
 
         context['graph'] = div
 
         return context
-
-
-def draw():
-
-    df = pd.read_csv(
-        "https://raw.githubusercontent.com/plotly/datasets/master/finance-charts-apple.csv")
-
-    trace_high = go.Scatter(
-        x=df.Date,
-        y=df['AAPL.High'],
-        name="AAPL High",
-        line=dict(color='#17BECF'),
-        opacity=0.8)
-
-    trace_low = go.Scatter(
-        x=df.Date,
-        y=df['AAPL.Low'],
-        name="AAPL Low",
-        line=dict(color='#7F7F7F'),
-        opacity=0.8)
-
-    data = [trace_high, trace_low]
-
-    layout = dict(
-        title="Manually Set Date Range",
-        xaxis=dict(
-            range=['2016-07-01', '2016-12-31'])
-    )
-
-    fig = dict(data=data, layout=layout)
-    py.iplot(fig, filename="Manually Set Range")
